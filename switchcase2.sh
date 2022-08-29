af284fbbfd6e52a4a1b4728e9183a72ab885c6e9
KBhanuDeepthi <bhanudeepthivinni915@gmail.com>
2022-08-29 15:44:57 +0530

    Added: Switchcase logic

---
+++ switchcase2.sh
@@ -0,0 +1,12 @@
+read -p "enter the num:" num
+case $num in
+     1)echo "unit"
+          ;;
+     10)echo "ten"
+          ;;
+     100)echo "Hundred"
+          ;;
+     1000)echo "Thousand"
+          ;;
+     *)echo "Invalid Number"
+esac
